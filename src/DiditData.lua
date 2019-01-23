f296c65fd0b4ff698d5cc5d9ea67f8e5963e8756
opussf <github@zz9-za.com>
2019-01-23 14:58:25 -0800

    Including LK and Cata

--- src/DiditData.lua
+++ src/DiditData.lua
@@ -60,6 +60,59 @@ Didit.data = {
 	["Hellfire Ramparts"] =          { ["Normal"] = 6147 },
 	["Battle for Mount Hyjal"] =     { ["Normal"] = 6148 },
 
+	-- WoLK
+	[14823] = "WotLK",
+	["The Nexus"] =                  { ["Normal"] = 1231, ["Heroic"] = 1505 },
+	["Azjol-Nerub"] =                { ["Normal"] = 1232, ["Heroic"] = 1506 },
+	["Ahn'kahet: The Old Kingdom"] = { ["Normal"] = 1233, ["Heroic"] = 1507 },
+	["Drak'Tharon Keep"] =           { ["Normal"] = 1234, ["Heroic"] = 1508 },
+	["The Violet Hold"] =            { ["Normal"] = 1235, ["Heroic"] = 1509 },
+	["Gundrak"] =                    { ["Normal"] = 1236, ["Heroic"] = 1510 },
+	["Halls of Stone"] =             { ["Normal"] = 1237, ["Heroic"] = 1511 },
+	["Halls of Lightning"] =         { ["Normal"] = 1238, ["Heroic"] = 1512 },
+	["The Oculus"] =                 { ["Normal"] = 1239, ["Heroic"] = 1513 },
+	["Utgarde Pinnacle"] =           { ["Normal"] = 1240, ["Heroic"] = 1514 },
+	["The Culling of Stratholme"] =  { ["Normal"] = 1241, ["Heroic"] = 1515 },
+	["Utgarde Keep"] =               { ["Normal"] = 1242, ["Heroic"] = 1504 },
+
+	["Trial of the Champion"] =      { ["Normal"] = 4026, ["Heroic"] = 4027 },
+
+	["The Forge of Souls"] =         { ["Normal"] = 4715, ["Heroic"] = 4716 },
+	["Halls of Reflection"] =        { ["Normal"] = 4726, ["Heroic"] = 4727 },
+	["Pit of Saron"] =               { ["Normal"] = 4720, ["Heroic"] = 4721 },
+
+	-- Raids
+	["Icecrown Citadel"] =           { ["Normal"] = 4653, ["Heroic"] = 4686 },  -- 10 player
+
+
+	-- Cata
+	[15096] = "Cata",
+	["Blackrock Caverns"] =        { ["Normal"] = 5724, ["Heroic"] = 5725 },
+	["Throne of the Tides"] =      { ["Normal"] = 5726, ["Heroic"] = 5727 },
+	["The Stonecore"] =            { ["Normal"] = 5728, ["Heroic"] = 5729 },
+	["The Vortex Pinnacle"] =      { ["Normal"] = 5730, ["Heroic"] = 5731 },
+	["Grim Batol"] =               { ["Normal"] = 5732, ["Heroic"] = 5733 },
+	["Halls of Origination"] =     { ["Normal"] = 5734, ["Heroic"] = 5735 },
+	["Lost City of the Tol'vir"] = { ["Normal"] = 5736, ["Heroic"] = 5737 },
+
+
+
+
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+	[""] = { ["Normal"] = 0, ["Heroic"] = 0 },
+
+
+
 
 
 	-- BfA
@@ -77,48 +130,6 @@ Didit.data = {
 
 
 
-
-
-	["14823"] = "WotLK",
-	-- WoLK
-	["Utgarde Keep"]               = function(isHeroic) return (isHeroic and 1504 or 1242); end,
-	["The Nexus"]                  = function(isHeroic) return (isHeroic and 1505 or 1231); end,
-	["Azjol-Nerub"]                = function(isHeroic) return (isHeroic and 1506 or 1232); end,
-	["Ahn'kahet: The Old Kingdom"] = function(isHeroic) return (isHeroic and 1507 or 1233); end,
-	["Drak'Tharon Keep"]           = function(isHeroic) return (isHeroic and 1508 or 1234); end,
-	["The Violet Hold"]            = function(isHeroic) return (isHeroic and 1509 or 1235); end,
-	["Gundrak"]                    = function(isHeroic) return (isHeroic and 1510 or 1236); end,
-	["Halls of Stone"]             = function(isHeroic) return (isHeroic and 1511 or 1237); end,
-	["Halls of Lightning"]         = function(isHeroic) return (isHeroic and 1512 or 1238); end,
-	["The Oculus"]                 = function(isHeroic) return (isHeroic and 1513 or 1239); end,
-	["Utgarde Pinnacle"]           = function(isHeroic) return (isHeroic and 1514 or 1240); end,
-	["The Culling of Stratholme"]  = function(isHeroic) return (isHeroic and 1515 or 1241); end,
-	["Trial of the Champion"]      = function(isHeroic) return (isHeroic and 4027 or 4026); end,
-	["The Forge of Souls"]         = function(isHeroic) return (isHeroic and 4716 or 4715); end,
-	["Pit of Saron"]               = function(isHeroic) return (isHeroic and 4721 or 4720); end,
-	["Halls of Reflection"]        = function(isHeroic) return (isHeroic and 4727 or 4726); end,
-	-- Raids
-	["Icecrown Citadel"]           = function(isHeroic) return (isHeroic and 4686 or 4653); end,
-
-	["15096"] = "Cata",
-	["Blackrock Caverns"]        = function(isHeroic) return (isHeroic and 5725 or 5724); end,
-	["Throne of the Tides"]      = function(isHeroic) return (isHeroic and 5727 or 5726); end,
-	["The Stonecore"]            = function(isHeroic) return (isHeroic and 5729 or 5728); end,
-	["The Vortex Pinnacle"]      = function(isHeroic) return (isHeroic and 5731 or 5730); end,
-	["Grim Batol"]               = function(isHeroic) return (isHeroic and 5733 or 5732); end,
-	["Halls of Origination"]     = function(isHeroic) return (isHeroic and 5735 or 5734); end,
-	["Lost City of the Tol'vir"] = function(isHeroic) return (isHeroic and 5737 or 5736); end,
-	--[""] = function(isHeroic) return (isHeroic and ); end,
---	[] = function(isHeroic) return (isHeroic and ); end,
---	[] = function(isHeroic) return (isHeroic and ); end,
--- Patch 4.2.0
---
---	["End Time"]={"Murozond"},
---	["End Time - Herioc"]={"Murozond"},
---	["Well of Eternity"]={"Peroth'arn","Azshara","Mannoroth","Varo'then"},
---	["Well of Eternity - Heroic"]={"Peroth'arn","Azshara","Mannoroth","Varo'then"},
---	["Hour of Twilight"]={"Arcurion","Asira Dawnslayer","Archbishop Benedictus"},
---	["Hour of Twilight - Heroic"]={"Arcurion","Asira Dawnslayer","Archbishop Benedictus"},
 
 -- WoD
 	["15233"] = "WoD",
